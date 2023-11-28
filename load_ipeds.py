edf78f952e4d9c29fe6bfafcfccda2cd08512525
Lizzzwu <144729704+Lizzzwu@users.noreply.github.com>
2023-11-27 22:57:53 -0500

    final final file

--- load_ipeds.py
+++ load_ipeds.py
@@ -2,168 +2,107 @@ import pandas as pd
 import psycopg
 import sys
 import re
-import os
 import credentials
-from collections import Counter
-
-
-def write_invalid_row_to_csv(row, file_path='invalid_rows.csv'):
-    """Write invalid rows to a separate CSV file."""
-    with open(file_path, 'a') as f:
-        f.write(','.join(str(v) for v in row) + '\n')
 
 
 def connect_to_database():
-    """
-    Connect to the PostgreSQL database using psycopg.
-
-    Returns:
-        A tuple containing the psycopg connection and cursor objects.
-    """
+    # Establishing a connection to the database
     conn = psycopg.connect(
-        host="pinniped.postgres.database.azure.com",
-        dbname="xwu5",
+        host = "pinniped.postgres.database.azure.com",
+        dbname = credentials.DB_USER,
         user=credentials.DB_USER,
         password=credentials.DB_PASSWORD
     )
 
     cur = conn.cursor()
+    return conn,cur
 
-    return conn, cur
 
-
-def clean_csv(csv_file_path):
-    """Clean the CSV data and add a 'year' column."""
-    df = pd.read_csv(csv_file_path, encoding='ISO-8859-1')
-
-    year_match = re.search(r'\d{4}', csv_file_path)
-    year = year_match.group(0) if year_match else 'Unknown'
+def read_csv(filename):
+    # Extracting year from the filename using regular expression
+    year = re.findall(r'\d{4}', filename)[0]
+    df = pd.read_csv(filename, encoding='ISO-8859-1', na_values=['', -999])
+    # Selecting specific variables from the dataframe
+    df = df[['UNITID', 'INSTNM', 'ADDR', 'CONTROL', 'CCBASIC', 'LATITUDE', 'LONGITUD']]
+    df = df.replace(-999, None)
+    # Adding a 'year' column to the dataframe
     df['year'] = year
-
-    for col in df.select_dtypes(include='object').columns:
-        df[col] = df[col].replace(['-2', '-1'], pd.NA)
-        df[col] = df[col].replace(r'[^\w\s]', '', regex=True)
-
-    for col in df.select_dtypes(include=['int64', 'float64']).columns:
-        df[col] = df[col].astype(str).replace(r'[^\d\.-]', '', regex=True)
-        df[col] = pd.to_numeric(df[col], errors='coerce')
-
-    return df
+    return df, year
 
 
-def infer_column_types(df, threshold=0.9):
-    """
-    Infer the expected data type for each column in the DataFrame.
-
-    Args:
-        df: Pandas DataFrame.
-        threshold: The percentage of values in a column that must be of
-        a certain type to infer it as the column type.
-
-    Returns:
-        A dictionary with column names as keys and inferred data types.
-    """
-    expected_types = {}
-    for column in df.columns:
-        types_count = Counter(df[column].dropna().map(type))
-        total_count = sum(types_count.values())
-
-        most_common_type, count = types_count.most_common(1)[0]
-        if count / total_count >= threshold:
-            expected_types[column] = most_common_type
-        else:
-            expected_types[column] = str
-
-    return expected_types
+def data_type(pandas_dtype):
+    # Mapping pandas dtypes to SQL data types
+    if pd.api.types.is_integer_dtype(pandas_dtype):
+        return "INTEGER"
+    elif pd.api.types.is_float_dtype(pandas_dtype):
+        return "FLOAT"
+    else:
+        return "TEXT"
 
 
-def create_table(df, table_name):
-    """Create a table in the database based on the DataFrame structure."""
-    if not isinstance(df, pd.DataFrame):
-        raise ValueError("df is not a DataFrame")
-
-    if df.empty:
-        raise ValueError("df is empty")
-
-    conn, cur = connect_to_database()
-    column_types = infer_column_types(df)
-
-    columns = []
-    for col, dtype in column_types.items():
-        column_type = f'{col} TEXT'
-        if dtype == int:
-            column_type = f'{col} INTEGER'
-        elif dtype == float:
-            column_type = f'{col} FLOAT'
-        columns.append(column_type)
-
+def create_table(df, table_name, conn, cur):
+    # Creating SQL column definitions based on dataframe dtypes
+    columns = [f"{col} {data_type(df[col].dtype)}" for col in df.columns]
     columns_str = ', '.join(columns)
     cur.execute(f"DROP TABLE IF EXISTS {table_name};")
     cur.execute(f'CREATE TABLE {table_name} ({columns_str});')
     conn.commit()
-    cur.close()
 
 
-def insert_data(df, table_name, expected_types):
-    """Insert data into the table and handle invalid rows."""
-    conn, cur = connect_to_database()
+def insert_data(df, table_name, conn, cur):
+    total_rows = len(df)
     inserted_rows = 0
-    rejected_rows = 0
+    failed_rows = 0
+    failed_data = []
 
-    invalid_rows_index = pd.Index([])
-    for column_name, expected_type in expected_types.items():
-        invalid_rows = df[~df[column_name].apply(
-            lambda x: isinstance(x, expected_type) or pd.isna(x))]
-        invalid_rows_index = invalid_rows_index.union(invalid_rows.index)
-
-    if not invalid_rows_index.empty:
-        rejected_rows_df = df.loc[invalid_rows_index]
-        rejected_rows_df.to_csv('rejected_rows.csv', index=False)
-        rejected_rows = len(invalid_rows_index)
-        df = df.drop(invalid_rows_index)
-
+    # Replacing pandas NA values with None for SQL compatibility
     df = df.replace({pd.NA: None})
-
     try:
-        with conn.transaction():
-            for index, row in df.iterrows():
-                placeholders = ', '.join(['%s'] * len(row))
-                columns = ', '.join(row.index)
-                sql = (
-                    f"INSERT INTO {table_name} "
-                    f"({columns}) "
-                    f"VALUES ({placeholders})"
-                )
+        for index, row in df.iterrows():
+            placeholders = ', '.join(['%s'] * len(row))
+            columns = ', '.join(row.index)
+            sql = (
+                f"INSERT INTO {table_name} "
+                f"({columns}) "
+                f"VALUES ({placeholders})"
+            )
+            try:
                 cur.execute(sql, list(row.values))
-            inserted_rows = len(df)
+                inserted_rows += 1
+            except Exception as e:
+                print(f"Error in row {index}: {e}")
+                failed_rows += 1
+                failed_data.append(row)
+                continue
+        conn.commit()
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Error during insertion: {e}")
+        conn.rollback()
         inserted_rows = 0
+        failed_rows = total_rows
 
-    conn.commit()
-    return inserted_rows, rejected_rows
+    # Writing failed data rows to a CSV file
+    if failed_rows > 0:
+        pd.DataFrame(failed_data).to_csv('failed_data.csv', index=False)
 
-
-def main(csv_file_path):
-    """Main function to process the CSV file and update the database."""
-    df = clean_csv(csv_file_path)
-    base_name = os.path.basename(csv_file_path)
-    table_name, _ = os.path.splitext(base_name)
-
-    create_table(df, table_name)
-    expected_types = infer_column_types(df)
-    conn, cur = connect_to_database()
-    inserted_rows, rejected_rows = insert_data(df, table_name, expected_types)
-    conn.close()
-
-    print(f"Total rows from CSV: {len(df)}")
-    print(f"Inserted: {inserted_rows}, Rejected: {rejected_rows}")
+    return total_rows, inserted_rows, failed_rows
 
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Usage: <csv_file_path>")
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python load-ipeds.py <filename>")
         sys.exit(1)
 
-    csv_file_path = sys.argv[1]
-    main(csv_file_path)
+    filename = sys.argv[1]
+    df, year = read_csv(filename)
+    table_name = f'ipeds_{year}'
+    conn, cur = connect_to_database()
+    create_table(df, table_name, conn, cur)
+    total_rows, inserted_rows, failed_rows = insert_data(df, table_name, conn, cur)
+    print(f"Total rows read from CSV: {total_rows}")
+    print(f"Total rows successfully inserted: {inserted_rows}")
+    print(f"Total rows failed to insert: {failed_rows}")
+
+    cur.close()
+    conn.close()
+
